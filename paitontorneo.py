26c0f59dda21d02094b579e4e86bb07fdc5864f5
cumax <maximo.contreras6k@gmail.com>
2024-10-07 11:35:58 -0300

    Torneo videojuegos 07-10-2024

--- paitontorneo.py
+++ paitontorneo.py
@@ -4,6 +4,7 @@
 
 # Ingresar 10 usuarios o participantes
 # Cada usuario tiene:
+# Nombre, nickname, edad, ranking, género, país y puntaje
 
 # Nombre participante local
 # Nombre participante visita
@@ -13,3 +14,28 @@
 # Match hasta que un usuario llegue a los 30 ptos.
 
 # Mostrar PODIUM del torneo (ranking, nickname y puntos)
+
+def validar_edad():
+    a
+
+
+
+
+nickname = [0,0,0,0,0,0,0,0,0,0]
+nombre = [0,0,0,0,0,0,0,0,0,0]
+edad =  [0,0,0,0,0,0,0,0,0,0]
+ranking = [0,0,0,0,0,0,0,0,0,0]
+género = [0,0,0,0,0,0,0,0,0,0]
+país = [0,0,0,0,0,0,0,0,0,0]
+puntaje = [0,0,0,0,0,0,0,0,0,0]
+
+print("Bienvenido al Torneo de Papulandia VideoGames")
+print("")
+for i in range(10):
+    nombre[i]=input("Ingrese nickname del usuario")
+    nickname[i]=input("Ingrese el nickname del usuario")
+    edad[i]=input("Ingrese la edad del usuario")
+    género[i]=input("Ingrese el género del usuario")
+    país[i]=input("Ingrese el país del usuario")
+    ranking[i]=input("Ingrese el ranking del usuario")
+    puntaje[i]= 0
