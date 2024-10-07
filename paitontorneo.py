33de9bbb77e486d4ac672865757d6c5c02dd9432
cumax <maximo.contreras6k@gmail.com>
2024-10-07 10:56:09 -0300

    a

---
+++ paitontorneo.py
@@ -0,0 +1,15 @@
+# *** TORNEO VIDEOJUEGOS ***
+
+# Crear un programa en Python que de solución a la siguiente problematica:
+
+# Ingresar 10 usuarios o participantes
+# Cada usuario tiene:
+
+# Nombre participante local
+# Nombre participante visita
+# Elegir videojuego
+# (Rocket League, League of Legends o Valorant)
+# Al ganador se le entrega 5 puntos
+# Match hasta que un usuario llegue a los 30 ptos.
+
+# Mostrar PODIUM del torneo (ranking, nickname y puntos)
