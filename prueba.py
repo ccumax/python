93d317b361632b8c67a9e854ef1df149570d1ab6
cumax <maximo.contreras6k@gmail.com>
2024-10-10 10:46:37 -0300

    alaverga

---
+++ prueba.py
@@ -0,0 +1,21 @@
+# Crear un programa en Python para INSUCO STREAMING
+
+# - Ingresar N películas
+# - Cada película tiene:
+#       * Nombre de la película
+#       * Protagonista
+#       * Antagonista
+#       * Director
+#       * Género
+#       * Año de estreno
+#       * Cantidad de visualizaciones o vistas
+
+# - Crear una función para validar GÉNERO
+#       (Acción, Terror, Comedia, Romance e Infantil)
+# - Crear otra función para validar AÑO (2000-2024)
+# - Mostrar película más vista
+# - Mostrar película menos vista
+# - Calcular promedio de visualizaciones o vistas
+# - Sumar cantidad de películas de acción y almacenarlas en un nuevo ARRAY
+
+# Se debe implementar ARRAY o ARREGLO
